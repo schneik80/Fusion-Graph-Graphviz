f5e32c56a0030b9c4e9f56c8318ecf5460bd4536
Kevin Schneider <schneik80@gmail.com>
2023-01-06 09:44:42 -0800

    improve graph formatting

--- Export-Graphviz-Diagram.py
+++ Export-Graphviz-Diagram.py
@@ -9,6 +9,7 @@ import re
 
 # Performs a recursive traversal of an entire assembly structure.
 
+
 def traverseAssembly(parent, occurrences, currentLevel, inputString):
     for i in range(0, occurrences.count):
         occ = occurrences.item(i)
@@ -18,14 +19,16 @@ def traverseAssembly(parent, occurrences, currentLevel, inputString):
 
         parent1 = parent.rsplit(" ", 1)[0]  # trim version
 
-        foo = '"' + parent1 + '"->"' + foo2 + '"' + ' ;' + '\n'
+        foo = '"' + parent1 + '"->"' + foo2 + '"' + " ;" + "\n"
 
         inputString += foo
 
         if occ.childOccurrences:
             inputString = traverseAssembly(
-                occ.name, occ.childOccurrences, currentLevel + 1, inputString)
+                occ.name, occ.childOccurrences, currentLevel + 1, inputString
+            )
     return inputString
+
 
 def run(context):
     ui = None
@@ -36,7 +39,7 @@ def run(context):
         product = app.activeProduct
         design = adsk.fusion.Design.cast(product)
         if not design:
-            ui.messageBox('No active Fusion design', 'No Design')
+            ui.messageBox("No active Fusion design", "No Design")
             return
 
         # Get the root component of the active design.
@@ -44,31 +47,43 @@ def run(context):
 
         # Create the title for the output.
         parentOcc = design.parentDocument.name
-        resultString = 'digraph' + ' {' + '\n' + 'layout="fdp";' + '\n' # Change layout engine here
-        #resultString += parentOcc + '\n'
+
+        resultString = (
+            'digraph "' + parentOcc + '" {' + "\n"
+        )  # Change layout engine here
+        resultString += 'layout="fdp";' + "\n"
+        resultString += "node[width=.75,height=.5,fontsize=9]" + "\n"
+        resultString += "nodesep=.2" + "\n"
+        resultString += (
+            "concentrate=false" + "\n"
+        )  # true draw one line, false draw lines for each occurrence
+        resultString += 'mode="ipsep"' + "\n"
+        resultString += "diredgeconstraints=true" + "\n"
+        resultString += 'overlap="false"' + "\n"
 
         # Call the recursive function to traverse the assembly and build the output string.
         resultString = traverseAssembly(
-            parentOcc, rootComp.occurrences.asList, 1, resultString)
+            parentOcc, rootComp.occurrences.asList, 1, resultString
+        )
 
-        resultString = resultString + '}'
+        resultString += "}"
 
-        msg = ''
+        msg = ""
         # Set styles of file dialog.
         folderDlg = ui.createFolderDialog()
-        folderDlg.title = 'Choose Folder to save graphviz Graph'
+        folderDlg.title = "Choose Folder to save graphviz Graph"
 
         # Show file save dialog
         dlgResult = folderDlg.showDialog()
         if dlgResult == adsk.core.DialogResults.DialogOK:
             filepath = os.path.join(folderDlg.folder, parentOcc + ".dot")
             # Write the results to the file
-            with open(filepath, 'w') as f:
+            with open(filepath, "w") as f:
                 f.write(resultString)
-            ui.messageBox('Graph saved at: ' + filepath)
+            ui.messageBox("Graph saved at: " + filepath)
         else:
             return
 
     except:
         if ui:
-            ui.messageBox('Failed:\n{}'.format(traceback.format_exc()))
+            ui.messageBox("Failed:\n{}".format(traceback.format_exc()))
